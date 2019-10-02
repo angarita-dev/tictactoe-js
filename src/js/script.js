1cdb3f5ff7248479b6cc970429cab43edfdc4b3e
ngodi <albertngodi@gmail.com>
2019-10-02 22:28:30 +0100

    game board view setup

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,10 @@ const Player = (name, character) =>{
     this.character = character;
 }
 
-const GameBoard = {
+const GameData = {
     board:[0,1,2,3,4,5,6,7,8]
 }
 
 const GameController = {
-    
+
 }
