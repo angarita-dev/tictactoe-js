342a3d46bbc66255fd14ac26beb38651d5ffe434
ngodi <albertngodi@gmail.com>
2019-10-02 22:13:33 +0100

    player objects and game data objects

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const Player = (name, character) =>{
+    this.name = name;
+    this.character = character;
+}
+
+const GameBoard = {
+    board:[0,1,2,3,4,5,6,7,8]
+}
+
+const GameController = {
+    
+}
