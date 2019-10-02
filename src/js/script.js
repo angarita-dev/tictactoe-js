12a87e367735c9bdd35ab8a91d3c13af3c9b36a4
ngodi <albertngodi@gmail.com>
2019-10-02 23:04:05 +0100

    game board DOM setup

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,31 @@
-const Player = (name, character) =>{
-    this.name = name;
+const Player = (character,turn) =>{
     this.character = character;
+    this.turn = turn;
 }
 
-const GameData = {
-    board:[0,1,2,3,4,5,6,7,8]
+const GameData = () =>{
+    board:[0,1,2,3,4,5,6,7,8];
 }
 
-const GameController = {
+const GameController = () => {
+ let board = new GameData;
+ let player = {
+    X :new Player("X",true),
+    O :new Player("O",false)
+ }
+}
+
+
+function move(playerCharacter, position){
+    let position = document.getElementById("position");
+    board[position] = player.character;
+
+}
+
+function validMoves(){
+
+}
+
+function gameStatus(){
 
 }
