1bc7934480238b26734735dd5c1c85e2843b709a
ngodi <albertngodi@gmail.com>
2019-10-05 01:11:05 +0100

    gamewin check function and board reset function

--- src/js/script.js
+++ src/js/script.js
@@ -49,10 +49,14 @@ if (board[index] == null) {
   turn = decideTurn(player1, player2);
   board[index] = turn.character;
   document.getElementById(id).innerHTML = turn.character;
- alert((board[0] == board[1])&&(board[1] == board[2]));
+ if(gameWin()){
+   alert(`${turn.name} WINS!!!`);
+   resetBoard();
+ }
   moveCount++;
   if (moveCount == 9){
-    return alert("gameOver");
+    alert("gameOver");
+    resetBoard();
   }
 }else{
   alert("cell occupied");
@@ -60,20 +64,23 @@ if (board[index] == null) {
 }
 
 function gameWin() {
-  if(
-    ( (board[3] == board[4])&&(board[4] == board[5]) )
-   )
-    /*
-    ((board[6] == board[7])&&(board[7] == board[8]))||
-    ((board[0] == board[3])&&(board[3] == board[6]))||
-    ((board[1] == board[4])&&(board[4] == board[7]))||
-    ((board[2] == board[5])&&(board[5] == board[8]))||
-    ((board[0] == board[4])&&(board[4] == board[8]))||
-    ((board[2] == board[4])&&(board[4] == board[6]))
-   ) */ {
-    return true;
-  }else{
-    return false;
-  }
+  if((board[0] != null && board[0] == board[1] && board[1] == board[2]) ||
+    (board[3] != null && board[3] == board[4] && board[4] == board[5]) ||
+    (board[6] != null && board[6] == board[7] && board[7] == board[8]) ||
+    (board[0] != null && board[0] == board[3] && board[3] == board[6]) ||
+    (board[1] != null && board[1] == board[4] && board[4] == board[7]) ||
+    (board[2] != null && board[2] == board[5] && board[5] == board[8]) ||
+    (board[0] != null && board[0] == board[4] && board[4] == board[8]) ||
+    (board[2] != null && board[2] == board[4] && board[4] == board[6]) 
+    )
+    {
+     return true;
+   }else{
+     return false;
+   }
 
+}
+
+function resetBoard(){
+  board.forEach(b => b = null);
 }
