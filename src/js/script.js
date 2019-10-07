296a0952daeca1f3f6f09e380115f1ea01143604
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-07 16:25:01 -0500

    domController refactor

--- src/js/script.js
+++ src/js/script.js
@@ -57,24 +57,35 @@ const domController = (() =>{
   const displayTurn = (name) => {
     document.getElementById("turn-message").innerHTML = `${name}'s turn`;
   }
-  const displayWin = () => {
-    document.getElementById("win-message").innerHTML = `${turn.name} WINS!!!`;
-    document.getElementsByClassName("reset-game-btn")[0].style.display = 'block';
+  const displayWin = (name) => {
+    document.getElementById("win-message").innerHTML = `${name} WINS!!!`;
+    document.getElementsByClassName("game-buttons-container")[0].style.display = 'flex';
   }
   const displayTie = () => {
     document.getElementById("win-message").innerHTML = `GAME OVER: YOU HAVE A TIE!!`;
-    document.getElementsByClassName("reset-game-btn")[0].style.display = 'block';
+    document.getElementsByClassName("game-buttons-container")[0].style.display = 'flex';
   }
   const clearBoard = () => {
     for(let i=0; i<9; i++){
       document.getElementById(`${i}`).innerHTML = null;
     }
   }
+  const clearWin = () => {
+    document.getElementById("win-message").innerHTML = null;
+  }
+  const resetBoard = () => {
+    clearBoard();
+    clearWin();
+    document.getElementsByClassName("game-buttons-container")[0].style.display = 'none';
+  }
   const getPlayerNames = () => {
     return [document.getElementById('name-player1').value,document.getElementById('name-player2').value]
   }
   const getPlayerChars = () => {
     return [document.getElementById('char-player1').value,document.getElementById('char-player2').value]
+  }
+  const hidePlayers = () => {
+    document.getElementsByClassName('player-data')[0].innerHTML = '';
   }
   return { getPlayerInfo, 
            toggleBoard, 
@@ -85,7 +96,9 @@ const domController = (() =>{
            displayTie,
            clearBoard,
            getPlayerNames,
-           getPlayerChars
+           getPlayerChars,
+           resetBoard,
+           hidePlayers
           };
 });
 
